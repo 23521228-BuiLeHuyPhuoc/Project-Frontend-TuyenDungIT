40713adb086a1ae30d339a5c26df1c6e0c61b3a0
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-03-17 00:38:22 +0700

    Tich hop trang dang nhap

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -27,15 +27,15 @@ export const Header = () => {
             {/* Account */}
             <div className="inline-flex items-center gap-x-[5px] text-white font-[600] sm:text-[16px] text-[12px] relative group/sub-1">
               {/* Chưa đăng nhập */}
-              {/* <Link href="#" className="">
+              <Link href="#" className="">
                 Đăng Nhập
               </Link>
               <span className="">/</span>
               <Link href="#" className="">
                 Đăng Ký
-              </Link> */}
+              </Link>
 
-              {/* Đã đăng nhập */}
+              {/* Đã đăng nhập
               <Link href="#" className="">LG Elect...</Link>
               <ul className="absolute top-[100%] right-[0px] w-[200px] bg-[#000065] hidden group-hover/sub-1:block z-[999]">
                 <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
@@ -58,7 +58,7 @@ export const Header = () => {
                     Đăng xuất
                   </Link>
                 </li>
-              </ul>
+              </ul> */}
             </div>
             {/* Button Menu Mobile */}
             <button 
---
+++ src/types/just-validate.d.ts
@@ -0,0 +1,5 @@
+
+declare module "just-validate" {
+  const JustValidate: any;
+  export default JustValidate;
+}
