877adc9e123c52a0685b4c2caf07d46136a401b9
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-04-10 11:02:20 +0700

    Them variable positionList va working formlist

---
+++ config/variable.ts
@@ -0,0 +1,57 @@
+export const positionList = [
+    {
+        label:"Intern",
+        value:"intern"
+    },
+    {
+        label:"Fresher",
+        value:"fresher"
+    },
+    {
+        label:"Junior",
+        value:"junior"
+    },
+    {
+        label:"Middle",
+        value:"middle"
+    },
+    {
+        label:"Senior",
+        value:"senior"
+    },
+    {
+        label:"Lead",
+        value:"lead"
+    },
+    {
+        label:"Manager",
+        value:"manager"
+    },
+    {
+        label:"Director",
+        value:"director"
+    },
+    {
+        label:"CEO",
+        value:"ceo"
+    },
+    {
+        label:"Other",
+        value:"other"
+    }
+
+]
+export const workingFromList=[
+    {
+        label:"Từ xa",
+        value:"remote"
+    },
+    {
+        label:"Tại văn phòng",
+        value:"office"
+    },
+    {
+        label:"Linh hoạt",
+        value:"hybrid"
+    }
+]
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -19,7 +19,6 @@ export const useAuth = () => {
         }
 
         if(data.code == "success") {
-          console.log(data);
           setIsLogin(true);
 
           if(data.infoUser) {
