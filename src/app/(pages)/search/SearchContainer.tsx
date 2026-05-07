c4b8eeee2d814ff144aa10d73d6cded2e7923afc
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-05-07 11:58:49 +0700

    Them tim kiem bang truong hinh thuc lam viec

--- src/app/(pages)/search/SearchContainer.tsx
+++ src/app/(pages)/search/SearchContainer.tsx
@@ -3,27 +3,28 @@
 import { CardJobItem } from "@/app/components/card/CardJobItem"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react";
-import { positionList } from "../../../../config/variable";
+import { positionList, workingFromList } from "../../../../config/variable";
 
 export const SearchContainer = () => {
   const router=useRouter();
   const searchParams = useSearchParams();
   const language = searchParams.get("language") || "";
   const city = searchParams.get("city") || "";
+  const workingFrom = searchParams.get("workingFrom") || "";
   const company = searchParams.get("company") || "";
   const keyword = searchParams.get("keyword") || "";
   const position= searchParams.get("position") || "";
   const [jobList, setJobList] = useState<any[]>([]);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/search?language=${language}&city=${city}&company=${company}&keyword=${keyword}&position=${position}`)
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/search?language=${language}&city=${city}&company=${company}&keyword=${keyword}&position=${position}&workingFrom=${workingFrom}`)
       .then(res => res.json())
       .then(data => {
         if(data.code == "success") {
           setJobList(data.jobs);
         }
       })
-  }, [language, city, company, keyword, position]);
+  }, [language, city, company, keyword, position, workingFrom]);
 
   const handleFilterPosition=(event:any)=>{
     const value=event.target.value;
@@ -37,6 +38,17 @@ export const SearchContainer = () => {
     }
     router.push(`?${params.toString()}`);
 
+  }
+  const handleFilterWorkingFrom=(event:any)=>{
+    const value=event.target.value;
+    const params=new URLSearchParams(searchParams.toString());
+    if(value) {
+      params.set("workingFrom", value);
+    }
+    else{
+      params.delete("workingFrom");
+    }
+    router.push(`?${params.toString()}`);
   }
 
   return (
@@ -66,11 +78,15 @@ export const SearchContainer = () => {
               <option key={index} value={position.value}>{position.label}</option>
             ))}
           </select>
-          <select name="" className="border border-[#DEDEDE] rounded-[20px] h-[36px] px-[18px] font-[400] text-[16px] text-[#414042]">
-            <option value="">Hình thức làm việc</option>
-            <option value="">Tại văn phòng</option>
-            <option value="">Làm từ xa</option>
-            <option value="">Linh hoạt</option>
+          <select name="" className="border border-[#DEDEDE] rounded-[20px] h-[36px] px-[18px] font-[400] text-[16px] text-[#414042]"
+          defaultValue={workingFrom}
+          onChange={handleFilterWorkingFrom}
+          >  
+          <option value="">Hình thức làm việc</option>
+
+            {workingFromList.map((item:any, index:number)=>(
+              <option key={index} value={item.value}>{item.label}</option>
+            ))}
           </select>
         </div>
 
